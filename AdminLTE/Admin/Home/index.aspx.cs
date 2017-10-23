2ca3742ec3ddeb892cb6bfd4272c24a4be994cb3
617050451 <617050451@qq.com>
2017-10-23 10:36:13 +0800

    111

--- AdminLTE/Admin/Home/index.aspx.cs
+++ AdminLTE/Admin/Home/index.aspx.cs
@@ -10,13 +10,14 @@ namespace AdminLTE.Admin
 {
     public partial class index : System.Web.UI.Page
     {
+        public string strJson = null;
         protected void Page_Load(object sender, EventArgs e)
         {
             DataTable dt = new DataTable();
             dt = BLL.BaseClass.getDataTable("SELECT * FROM [qds108295464_db].[dbo].[t_Mean]");
             BLL.AdminLTEHelper adminlte = new BLL.AdminLTEHelper();
             adminlte.GetTreeJsonByTable(dt, "GUID", "MeanName", "ParentID", "0", "MeanLevel");
-            string strJson = adminlte.result.ToString();
+            strJson = adminlte.result.ToString();
         }
     }
 }
